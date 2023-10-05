d244042e89997e080617700df6cfa78ccc4eeda0
denis <kamviktor1@ukr.net>
2023-10-05 13:15:20 +0300

    update

--- дз номер 5/дз номер 5.cpp
+++ дз номер 5/дз номер 5.cpp
@@ -1,7 +1,30 @@
 ﻿#include <iostream>
+#include<clocale>
+#include<string>
+
+using namespace std;
+
+class User {
+private:
+    int hp;
+    string name;
+    int age;
+public:
+    User(int hp, string name,int age) {
+        this->hp = hp;
+        this->name = name;
+        this->age = age;
+    }
+    void get_info() {
+        cout << "age: " << age << endl << "name: " << name << endl << "hp: " << hp << endl;
+    }
+};
 
 int main(){
+    setlocale(LC_ALL, "ru");
 
-    std::cout << "Myhomework5\n";
+    User alex(100, "alex", 15);
+    alex.get_info();
+
     return 0;
 }
